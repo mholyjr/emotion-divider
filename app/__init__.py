61721b8ee13293a2da86eb207483976e41f816d0
Martin Holý <m.holyjr@gmail.com>
2024-01-26 18:53:58 +0100

    Add: database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-import requests
-import os
 from app.controllers.openai_controller import OpenAIController
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    
     openai_controller = OpenAIController()
+    app.config.from_object(Config)
+
+    db.init_app(app)
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
@@ -14,5 +21,20 @@ def create_app():
             response = openai_controller.call_openai_api(inputData)
             return render_template('index.html', content=response)
         return render_template('index.html')
-      
+
+  
+    @app.route('/test_db')
+    def test_db():
+      try:
+        # Obtain a connection and execute a simple query
+        with db.engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+            for row in result:
+                print("Query result:", row)
+            return 'Database connection successful.'
+      except Exception as e:
+        # If an exception occurred, return the error
+        return f'Database connection failed: {e}'
+          
     return app
+
---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_STRING", "")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
